4b3c2739150b23aa055b8a869a0c9c3b6431fe3a
CosBoS-wedding-film <cosbos.wedding.film@gmail.com>
2024-08-21 01:40:09 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,3 +18,25 @@ function showSection(sectionId) {
         }
     });
 }
+
+
+
+function openTab(event, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Hide all tab content
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove active class from all tabs
+    tablinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the selected tab and add the active class
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
