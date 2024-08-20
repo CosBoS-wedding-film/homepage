6027b04a320c6c104a774813b815d8cc8abd9b6c
CosBoS-wedding-film <cosbos.wedding.film@gmail.com>
2024-08-21 00:49:43 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,14 +4,17 @@ function navigateToHomePage() {
 
 function showSection(sectionId) {
     const sections = document.querySelectorAll('.content-section');
-    
     sections.forEach(section => {
         if (section.id === sectionId) {
             section.style.display = 'block';
-            section.style.opacity = '1';
+            setTimeout(() => {
+                section.classList.add('show');
+            }, 0); // slight delay to trigger transition
         } else {
-            section.style.display = 'none';
-            section.style.opacity = '0';
+            section.classList.remove('show');
+            setTimeout(() => {
+                section.style.display = 'none';
+            }, 100); // match the CSS transition duration
         }
     });
 }
